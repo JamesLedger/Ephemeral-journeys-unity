8080b19518a7d429656a54a7b3094a2a265ade45
James Ledger <Jamesledger99@gmail.com>
2023-05-29 02:56:13 +0100

    Fixing cheese issue

--- Assets/Scripts/Items/ItemPickup.cs
+++ Assets/Scripts/Items/ItemPickup.cs
@@ -23,15 +23,4 @@ public class ItemPickup : Interactable
             Destroy(gameObject);
     }
 
-    private Color startcolor;
-    void OnMouseEnter()
-    {
-        startcolor = GetComponent<Renderer>().material.color;
-        GetComponent<Renderer>().material.color = Color.cyan;
-    }
-    void OnMouseExit()
-    {
-        GetComponent<Renderer>().material.color = startcolor;
-    }
-
 }
